42ca3b2ed7df13ebbfe5baa2901bbf4cff1f10f6
Santoshd561 <Santoshd561@gmail.com>
2019-07-22 20:50:13 +0530

    initial

---
+++ neha.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class neha extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default neha;
